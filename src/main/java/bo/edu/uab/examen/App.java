53a9ae87765c36f3e346ef6536a9af41db590b45
Victor <jvictorguzman8@gmail.com>
2022-02-07 11:38:41 -0400

    creacion de API REST

--- src/main/java/bo/edu/uab/examen/App.java
+++ src/main/java/bo/edu/uab/examen/App.java
@@ -1,13 +1,38 @@
 package bo.edu.uab.examen;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Convirtiendo objeto a JSON..." );
+        Properties properties = new Properties();
+        properties.load(App.class.getClassLoader().getResourceAsStream("vars.properties"));
+        Persona persona = new Persona();
+        persona.setNombre("Pedro Perez");
+        persona.setEdad(14);
+        persona.setPropiedad(properties.getProperty("PROPIEDAD_1"));
+        ObjectMapper mapper = new ObjectMapper();
+        String resultado = mapper.writeValueAsString(persona);
+        System.out.println(resultado);
+    }
+
+    public String obtenerJson(Persona persona) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        String resultado = mapper.writeValueAsString(persona);
+        
+        return resultado;
     }
 }
---
+++ src/main/java/bo/edu/uab/examen/Persona.java
@@ -0,0 +1,32 @@
+package bo.edu.uab.examen;
+
+
+public class Persona {
+    private String nombre;
+    private int edad;
+    private String propiedad;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getPropiedad() {
+        return propiedad;
+    }
+
+    public void setPropiedad(String propiedad) {
+        this.propiedad = propiedad;
+    }
+}
